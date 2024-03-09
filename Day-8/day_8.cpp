3e7ffcdfa3f33d8dc50fe18e514af782159dcb75
BPsaren <bishnupadasaren78@gmail.com>
2024-03-10 01:34:39 +0530

    Find the N-th characte

---
+++ Day-8/day_8.cpp
@@ -0,0 +1,30 @@
+//User function template for C++
+
+class Solution{
+  public:
+    char nthCharacter(string s, int r, int n) {
+        //code here
+        if (r==0){
+            return s[n];
+        }
+        char c=nthCharacter(s,r-1,n/2);
+            if(n%2==1){
+                //odd case
+                if(c=='0'){
+                    return '1';
+                }else{
+                    return '0';
+                }
+            }
+            else{
+                if(c=='0'){
+                    //even case
+                    return '0';
+                }else{
+                    return '1';
+                }
+            }
+       
+        
+    }
+};
